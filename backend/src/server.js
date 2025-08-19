48a6c06347a1ea147affbf7e47413cc5e9377091
TorqueReborn <torquereborn@gmail.com>
2025-08-19 23:31:18 +0530

    refactor: streamline anime data fetching and parsing logic

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import { connectAllAnime, connectSite } from './util.js';
+import { connectAllAnime, parseAnime } from './util.js';
 
 dotenv.config();
 const app = express();
@@ -18,22 +18,11 @@ app.listen(PORT, () => {
 });
 
 app.get('/latest', async (_, res) => {
-    const url = `${API_BASE}?variables={}&extensions={\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"06327bc10dd682e1ee7e07b6db9c16e9ad2fd56c1b769e47513128cd5c9fc77a\"}}`
+    let url = `${API_BASE}?query={shows{edges{_id,name,thumbnail}}}`
     const data = await connectAllAnime(url);
     if (data) {
-        const animes = data.data.shows.edges.map(edge => {
-            let thumbnail = edge.thumbnail;
-            if (thumbnail && !thumbnail.includes('http')) {
-                thumbnail = `https://wp.youtube-anime.com/aln.youtube-anime.com/${thumbnail}`;
-            }
-
-            return {
-                id: edge._id,   
-                name: edge.name,
-                thumbnail: thumbnail,
-            }
-        });
-        res.json(animes);
+        const rawJSON = await data.json();
+        res.json(parseAnime(rawJSON));
     } else {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
@@ -42,42 +31,6 @@ app.get('/latest', async (_, res) => {
 app.get('/server', async (_, res) => {
     const url = `https://api.allanime.day/api?variables={%22showId%22:%22ReooPAxPMsHM4KPMY%22,%22translationType%22:%22sub%22,%22episodeString%22:%221140%22}&extensions={%22persistedQuery%22:{%22version%22:1,%22sha256Hash%22:%225f1a64b73793cc2234a389cf3a8f93ad82de7043017dd551f38f65b89daa65e0%22}}`
     const data = await connectAllAnime(url);
-
-    const sourceUrls = data.data.episode.sourceUrls;
-    const test = sourceUrls.filter(source => source.sourceUrl.includes('--')).map(source => {
-        let url = source.sourceUrl
-        url = url.substring(2)
-            .match(/.{1,2}/g)
-            .map(hex => String.fromCharCode(parseInt(hex, 16) ^ 56))
-            .join('')
-            .replace('clock', 'clock.json');
-        url = url.includes('clock') ? `https://allanime.day${url}` : url;
-
-        return {
-            name: source.sourceName,
-            url: url,
-        }
-    })
-
-    const urls = []
-
-    for (const source of test) {
-        if (!source.url.includes('clock')) {
-            urls.push(source.url);
-            continue
-        }
-        const siteData = await connectSite(source.url);
-        if ('message' in siteData) {
-            continue
-        }
-        siteData.links.map(link => {
-            urls.push(link.link);
-        })
-    }
-
-    if (data) {
-        res.json(urls);
-    } else {
-        res.status(500).json({ error: 'Failed to fetch data' });
-    }
+    
+    res.json(data)
 });
--- backend/src/util.js
+++ backend/src/util.js
@@ -9,25 +9,19 @@ const connectAllAnime = async (url) => {
         if (!response.ok) {
             return { message: 'Failed to fetch data from AllAnime API', status: response.status };
         }
-        return await response.json();
+        return response
 
     } catch (error) {
         return { message: 'Error fetching data from AllAnime API', status: 500 };
     }
 }
 
-const connectSite = async (url) => {
-    try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-            return { message: 'Failed to fetch data from AllAnime API', status: response.status };
-        }
-        return await response.json();
-
-    } catch (error) {
-        return { message: 'Error fetching data from AllAnime API', status: 500 };
-    }
+const parseAnime = (rawJSON) => {
+    return rawJSON.data.shows.edges.map(anime => ({
+        id: anime._id,
+        name: anime.name,
+        thumbnail: anime.thumbnail.includes('http') ? anime.thumbnail : `https://wp.youtube-anime.com/aln.youtube-anime.com${anime.thumbnail}`
+    }))
 }
 
-export {connectAllAnime, connectSite};
+export { connectAllAnime, parseAnime };
