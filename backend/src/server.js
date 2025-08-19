43faf63125fb49b7d9f773865ef1377a6230f3a4
TorqueReborn <torquereborn@gmail.com>
2025-08-19 07:46:32 +0530

    feat: implement server endpoint for fetching and processing anime source URLs

--- backend/src/server.js
+++ backend/src/server.js
@@ -26,3 +26,30 @@ app.get('/latest', async (_, res) => {
         res.status(500).json({ error: 'Failed to fetch data' });
     }
 });
+
+app.get('/server', async (_, res) => {
+    const url = `https://api.allanime.day/api?variables={%22showId%22:%22ReooPAxPMsHM4KPMY%22,%22translationType%22:%22sub%22,%22episodeString%22:%221140%22}&extensions={%22persistedQuery%22:{%22version%22:1,%22sha256Hash%22:%225f1a64b73793cc2234a389cf3a8f93ad82de7043017dd551f38f65b89daa65e0%22}}`
+    const data = await connectAllAnime(url);
+
+    const sourceUrls = data.data.episode.sourceUrls;
+    const test = sourceUrls.filter(source => source.sourceUrl.includes('--')).map(source => {
+        let url = source.sourceUrl
+        url = url.substring(2)
+            .match(/.{1,2}/g)
+            .map(hex => String.fromCharCode(parseInt(hex, 16) ^ 56))
+            .join('')
+            .replace('clock', 'clock.json');
+        url = url.includes('clock') ? `https://allanime.day${url}` : url;
+
+        return {
+            name: source.sourceName,
+            url: url,
+        }
+    })
+
+    if (data) {
+        res.json(test);
+    } else {
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,8 @@
-import { useEffect, useState } from "react";
-import { latestAnime } from "./utils";
-
 const App = () => {
-  const [anime, setAnime] = useState<any>([]);
-
-  useEffect(() => {
-    const animes = latestAnime();
-    setAnime(animes);
-  }, []);
-
+  
   return (
     <div className="font-poppins">
-      {anime}
+      
     </div>
   )
 }
