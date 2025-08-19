3b483614aadee3666bcbc7e90f143959d7cb52fb
TorqueReborn <torquereborn@gmail.com>
2025-08-19 06:35:04 +0530

    feat: refactor anime data fetching into a separate utility function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,12 @@
 import { useEffect, useState } from "react";
-import Card from "./components/Card";
+import { latestAnime } from "./utils";
 
 const App = () => {
   const [anime, setAnime] = useState<any>([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/latest');
-        const result = await response.json();
-        const animes = result.data.shows.edges
-        .map((edge: any ) => {
-          let thumbnail = edge.thumbnail
-          if (!thumbnail.startsWith("https://")) {
-            thumbnail = 'https://wp.youtube-anime.com/aln.youtube-anime.com/' + thumbnail;
-          }
-          return <div key={edge._id}>
-            <Card name={edge.name} thumbnail={thumbnail}/>
-          </div>
-        })
-        setAnime(animes);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    }
-    fetchData();
+    const animes = latestAnime();
+    setAnime(animes);
   }, []);
 
   return (
---
+++ frontend/src/utils.tsx
@@ -0,0 +1,22 @@
+import Card from "./components/Card";
+
+export const latestAnime = async () => {
+    try {
+        const response = await fetch('http://localhost:3000/latest');
+        const result = await response.json();
+        const animes = result.data.shows.edges
+            .map((edge: any) => {
+                let thumbnail = edge.thumbnail
+                if (!thumbnail.startsWith("https://")) {
+                    thumbnail = 'https://wp.youtube-anime.com/aln.youtube-anime.com/' + thumbnail;
+                }
+                return <div key={edge._id}>
+                    <Card name={edge.name} thumbnail={thumbnail} />
+                </div>
+            })
+        return animes
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+    return null
+}
