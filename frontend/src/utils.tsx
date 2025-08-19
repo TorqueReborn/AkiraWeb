a96f81332f89710e338f78ae39b41b1f9270ae3c
TorqueReborn <torquereborn@gmail.com>
2025-08-19 06:38:28 +0530

    refactor: define Edge interface and simplify latestAnime function

--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -1,20 +1,25 @@
 import Card from "./components/Card";
 
+interface Edge {
+    _id: string;
+    name: string;
+    thumbnail: string;
+}
+
 export const latestAnime = async () => {
     try {
         const response = await fetch('http://localhost:3000/latest');
         const result = await response.json();
-        const animes = result.data.shows.edges
-            .map((edge: any) => {
-                let thumbnail = edge.thumbnail
-                if (!thumbnail.startsWith("https://")) {
-                    thumbnail = 'https://wp.youtube-anime.com/aln.youtube-anime.com/' + thumbnail;
-                }
-                return <div key={edge._id}>
-                    <Card name={edge.name} thumbnail={thumbnail} />
-                </div>
-            })
-        return animes
+        return result.data.shows.edges.map((edge: Edge) => {
+            let thumbnail = edge.thumbnail
+
+            if (!thumbnail.startsWith("https://")) {
+                thumbnail = 'https://wp.youtube-anime.com/aln.youtube-anime.com/' + thumbnail;
+            }
+            return <div key={edge._id}>
+                <Card name={edge.name} thumbnail={thumbnail} />
+            </div>
+        })
     } catch (error) {
         console.error("Error fetching data:", error);
     }
