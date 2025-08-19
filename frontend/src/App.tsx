bec29e6fdba8aed09fa9338d87f2e39d7741f222
TorqueReborn <torquereborn@gmail.com>
2025-08-19 08:55:37 +0530

    feat: implement splash screen logic to show/hide based on localStorage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import Splash from "./splash/Splash"
 
 const App = () => {
+  const splashSeen = localStorage.getItem("splashSeen");
 
   return (
     <div className="font-poppins">
-      <Splash />
+      {splashSeen ? 'Splash Seen' : <Splash />}
     </div>
   )
 }
--- frontend/src/splash/Splash.tsx
+++ frontend/src/splash/Splash.tsx
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
 import { AiFillRightCircle } from "react-icons/ai"
 
 const Splash = () => {
+    const [showSplash, setShowSplash] = useState(false);
+
+    useEffect(() => {
+        const splashSeen = localStorage.getItem("splashSeen");
+        if (!splashSeen) {
+            setShowSplash(true);
+        }
+    }, []);
+
+    const handleClick = () => {
+        localStorage.setItem("splashSeen", "true");
+        window.location.reload();
+    }
+    if (!showSplash) {
+        return null;
+    }
+
     return (
         <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
             <h1 className="text-6xl font-black bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
                 Akira
             </h1>
             <p>By Ghost Reborn!!!</p>
-            <AiFillRightCircle className="text-5xl mt-5 cursor-pointer"/>
+            <button onClick={handleClick}>
+                <AiFillRightCircle className="text-5xl mt-5 cursor-pointer"/>
+            </button>
         </div>
     )
 }
