426affa943b23985bf1fe1898510834b868785c0
TorqueReborn <torquereborn@gmail.com>
2025-08-21 18:32:24 +0530

    Move styles

--- frontend/src/login/Login.tsx
+++ frontend/src/login/Login.tsx
@@ -53,11 +53,11 @@ const Login = ({setIsLoggedIn}: LoginProps) => {
 
     return (
         <div className='h-screen flex justify-center items-center'>
-            <div className='bg-amber-300 items-center justify-center flex flex-col p-12 rounded-xl'>
+            <div className='bg-gray-800 items-center justify-center flex flex-col p-12 rounded-xl'>
                 <h1 className='text-3xl'>Login</h1>
                 <input className='mt-8 text-center border-b-1 w-8/10' type="text" placeholder='Enter email' onChange={handleEmailChange}/><br />
                 <input className='mt-6 text-center border-b-1 w-8/10' type="password" placeholder='Enter password' onChange={handlePasswordChange} />
-                <button className='mt-6 bg-white px-4 py-2 cursor-pointer rounded-2xl' type='submit' onClick={handleSubmit}>Login</button>
+                <button className='mt-6 px-4 py-2 cursor-pointer rounded-2xl' type='submit' onClick={handleSubmit}>Login</button>
             </div>
         </div>
     )
