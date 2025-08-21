3e25b6e92fca5886d3efe18f154a28e1988a73dd
TorqueReborn <torquereborn@gmail.com>
2025-08-21 08:41:14 +0530

    feat: add jsonwebtoken dependency for authentication handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+
 import connectDB from './config/connectDb.js';
 import authRouter from './routers/authRouter.js';
 
