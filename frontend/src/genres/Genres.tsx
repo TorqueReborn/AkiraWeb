4ea01a9b39754e78b885a12a65a55b1dbc837c45
TorqueReborn <torquereborn@gmail.com>
2025-08-22 04:02:23 +0530

    Genres Selection and Check button ui

--- frontend/src/genres/Genres.tsx
+++ frontend/src/genres/Genres.tsx
@@ -2,8 +2,17 @@ import Card from "./components/Card"
 
 const Genres = () => {
     return (
-        <div>
-            <Card name="One Piece" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250" />
+        <div className="flex flex-col h-screen items-center justify-center">
+            <div className="text-3xl mb-10">
+                What anime do you like?
+            </div>
+            <div className="flex gap-10">
+                <Card name="One Piece" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250" genre="Shonen" />
+                <Card name="Princess Jellyfish" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx8129-VasAsXbiixR1.jpg?w=250" genre="Josei" />
+                <Card name="Nmeneko" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx186313-29gCf3CWJu1n.jpg?w=250" genre="Comedy"/>
+                <Card name="Berserk" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21560-6iifjdssfebJ.jpg?w=250" genre="Seinen " />
+                <Card name="Sailor Moon" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx530-I0XN6WzeBtAg.jpg?w=250" genre="Shoujo" />
+            </div>
         </div>
     )
 }
--- frontend/src/genres/components/Card.tsx
+++ frontend/src/genres/components/Card.tsx
@@ -1,12 +1,19 @@
+import { IoMdCheckmarkCircle } from "react-icons/io"
+
 interface CardProps {
     name: string,
-    thumbnail: string
+    thumbnail: string,
+    genre: string
 }
 
-const Card = ({ name, thumbnail }: CardProps) => {
+const Card = ({ name, thumbnail, genre }: CardProps) => {
     return (
-        <div className='bg-gray-700 w-fit'>
-            <img className='w-48 h-72' src={thumbnail} alt="Anime Image" />
+        <div className='bg-gray-700 w-fit rounded'>
+            <h1 className="bg-gray-900 pb-2 text-xl">{genre}</h1>
+            <div className="relative">
+                <img className='w-48 h-72 rounded-t' src={thumbnail} alt="Anime Image" />
+                <IoMdCheckmarkCircle className="absolute top-0 right-0 text-white hidden bg-gray-800 h-8 w-8 p-1" />
+            </div>
             <h1 className='p-2 italic font-medium w-48 line-clamp-1 text-center text-xl'>{name}</h1>
         </div>
     )
