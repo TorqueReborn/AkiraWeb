19bec7fc8e3e3aaeaa75073167eb1ab48b26e49a
TorqueReborn <torquereborn@gmail.com>
2025-08-22 10:02:02 +0530

    Fix Genre Selection: Dumb me 😶‍🌫️

--- frontend/src/genres/Genres.tsx
+++ frontend/src/genres/Genres.tsx
@@ -1,3 +1,4 @@
+import { AiFillRightCircle } from 'react-icons/ai'
 import Card from './components/Card'
 import { useState } from 'react'
 
@@ -5,11 +6,19 @@ const Genres = () => {
     const [selectedGenres, setSelectedGenres] = useState([])
 
     return (
-        <div>
-            <Card genre='Soaikou' setSelectedGenres={setSelectedGenres} />
-            <Card genre='Something' setSelectedGenres={setSelectedGenres} />
-            <button onClick={() => console.log(selectedGenres)}>
-                Check
+        <div className="flex flex-col h-screen items-center justify-center">
+            <div className="text-3xl mb-20">
+                What anime do you like?
+            </div>
+            <div className="flex gap-10">
+                <Card name="One Piece" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250" genre="Shonen" setSelectedGenres={setSelectedGenres} />
+                <Card name="Princess Jellyfish" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx8129-VasAsXbiixR1.jpg?w=250" genre="Josei" setSelectedGenres={setSelectedGenres} />
+                <Card name="Nmeneko" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx186313-29gCf3CWJu1n.jpg?w=250" genre="Comedy" setSelectedGenres={setSelectedGenres} />
+                <Card name="Berserk" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21560-6iifjdssfebJ.jpg?w=250" genre="Seinen " setSelectedGenres={setSelectedGenres} />
+                <Card name="Sailor Moon" thumbnail="https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/nx530-I0XN6WzeBtAg.jpg?w=250" genre="Shoujo" setSelectedGenres={setSelectedGenres} />
+            </div>
+            <button className="rounded-full focus:outline-none focus:ring-1 focus:ring-gray-400 mt-20" onClick={() => console.log(selectedGenres)}>
+                <AiFillRightCircle className="text-5xl cursor-pointer" />
             </button>
         </div>
     )
--- frontend/src/genres/components/Card.tsx
+++ frontend/src/genres/components/Card.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react"
+import { IoMdCheckmarkCircle } from "react-icons/io"
 
 interface CardProps {
+    name: string,
+    thumbnail: string,
     genre: string,
     setSelectedGenres: Function
 }
 
-const Card = ({ genre, setSelectedGenres }: CardProps) => {
+const Card = ({ name, thumbnail, genre, setSelectedGenres }: CardProps) => {
     const [selected, setSelected] = useState(false)
 
     const handleClick = () => {
+        setSelected(!selected)
         setSelectedGenres((genres: string[]) => {
             if (genres.includes(genre)) {
                 return genres.filter((g) => g !== genre)
@@ -19,9 +23,14 @@ const Card = ({ genre, setSelectedGenres }: CardProps) => {
     }
 
     return (
-        <div onClick={handleClick} className={`${selected ? 'hidden' : ''}`}>
-            {genre}
-        </div>
+        <button className="focus:outline-none focus:scale-110 bg-gray-700 rounded" onClick={handleClick}>
+            <h1 className="bg-gray-900 pb-4 text-xl">{genre}</h1>
+            <div className="relative">
+                <img className='w-48 h-72 rounded-t' src={thumbnail} alt="Anime Image" />
+                <IoMdCheckmarkCircle className={`absolute top-0 right-0 text-white bg-gray-800 h-8 w-8 p-1 ${!selected && 'hidden'}`} />
+            </div>
+            <h1 className='p-2 italic font-medium w-48 line-clamp-1 text-center text-xl'>{name}</h1>
+        </button>
     )
 }
 
