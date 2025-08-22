b661f46c7b1e9ec7d1457f99ccc1f3f1d1c26836
TorqueReborn <torquereborn@gmail.com>
2025-08-22 07:02:07 +0530

    Genres Selection Drop: Code not good enough

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,15 @@ import { useState } from "react";
 import Login from "./login/Login";
 import Splash from "./splash/Splash"
 import Cookies from 'js-cookie'
-import Genres from "./genres/Genres";
 
 const App = () => {
   const [splashSeen, setSplashSeen] = useState(Cookies.get('splashSeen'))
   const [isLoggedIn, setIsLoggedIn] = useState(Cookies.get('token'))
-  const [selectedGenres, setSelectedGenres] = useState(Cookies.get('genres'))
 
   return (
     <div>
       {!splashSeen && <Splash setSplashSeen={setSplashSeen} />}
       {(!isLoggedIn && splashSeen) && <Login setIsLoggedIn={setIsLoggedIn} />}
-      {(isLoggedIn && splashSeen && !selectedGenres) && <Genres />}
     </div>
   )
 }
