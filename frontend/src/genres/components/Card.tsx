d717000ddc64c612a267f22a97ef94c4673cc320
TorqueReborn <torquereborn@gmail.com>
2025-08-22 06:26:44 +0530

    Stylize and why is there a div anyways

--- frontend/src/genres/components/Card.tsx
+++ frontend/src/genres/components/Card.tsx
@@ -16,15 +16,13 @@ const Card = ({ name, thumbnail, genre }: CardProps) => {
     }
 
     return (
-        <button className="focus:outline-none focus:scale-110 w-full bg-gray-700 rounded" onClick={handleClick}>
-            <h1 className="bg-gray-900 pb-2 text-xl">{genre}</h1>
-            <div>
-                <div className="relative">
-                    <img className='w-48 h-72 rounded-t' src={thumbnail} alt="Anime Image" />
-                    <IoMdCheckmarkCircle className={`absolute top-0 right-0 text-white bg-gray-800 h-8 w-8 p-1 ${!checked && 'hidden'}`} />
-                </div>
-                <h1 className='p-2 italic font-medium w-48 line-clamp-1 text-center text-xl'>{name}</h1>
+        <button className="focus:outline-none focus:scale-110 bg-gray-700 rounded" onClick={handleClick}>
+            <h1 className="bg-gray-900 pb-4 text-xl">{genre}</h1>
+            <div className="relative">
+                <img className='w-48 h-72 rounded-t' src={thumbnail} alt="Anime Image" />
+                <IoMdCheckmarkCircle className={`absolute top-0 right-0 text-white bg-gray-800 h-8 w-8 p-1 ${!checked && 'hidden'}`} />
             </div>
+            <h1 className='p-2 italic font-medium w-48 line-clamp-1 text-center text-xl'>{name}</h1>
         </button>
     )
 }
