6aa82b2b0d0f537432b7fd7f152e9101c7bf3d7d
TorqueReborn <torquereborn@gmail.com>
2025-08-22 06:10:18 +0530

    Click show checkmark

--- frontend/src/genres/components/Card.tsx
+++ frontend/src/genres/components/Card.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { IoMdCheckmarkCircle } from "react-icons/io"
 
 interface CardProps {
@@ -7,12 +8,19 @@ interface CardProps {
 }
 
 const Card = ({ name, thumbnail, genre }: CardProps) => {
+    const [checked, setChecked] = useState(false)
+
+    const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        console.log(e)
+        setChecked(!checked)
+    }
+
     return (
         <div className='bg-gray-700 w-fit rounded'>
             <h1 className="bg-gray-900 pb-2 text-xl">{genre}</h1>
-            <div className="relative">
+            <div className="relative" onClick={handleClick}>
                 <img className='w-48 h-72 rounded-t' src={thumbnail} alt="Anime Image" />
-                <IoMdCheckmarkCircle className="absolute top-0 right-0 text-white hidden bg-gray-800 h-8 w-8 p-1" />
+                <IoMdCheckmarkCircle className={`absolute top-0 right-0 text-white bg-gray-800 h-8 w-8 p-1 ${!checked && 'hidden'}`} />
             </div>
             <h1 className='p-2 italic font-medium w-48 line-clamp-1 text-center text-xl'>{name}</h1>
         </div>
