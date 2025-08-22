d324ac0e8a41acdf3a8863d0e7c1f3e05e1a77a0
TorqueReborn <torquereborn@gmail.com>
2025-08-22 11:01:50 +0530

    Show only anime name, ui looked ugly before

--- frontend/src/genres/components/Card.tsx
+++ frontend/src/genres/components/Card.tsx
@@ -24,12 +24,11 @@ const Card = ({ name, thumbnail, genre, setSelectedGenres }: CardProps) => {
 
     return (
         <button className="focus:outline-none focus:scale-110 bg-gray-700 rounded" onClick={handleClick}>
-            <h1 className="bg-gray-900 pb-4 text-xl">{genre}</h1>
             <div className="relative">
                 <img className='w-48 h-72 rounded-t' src={thumbnail} alt="Anime Image" />
                 <IoMdCheckmarkCircle className={`absolute top-0 right-0 text-white bg-gray-800 h-8 w-8 p-1 ${!selected && 'hidden'}`} />
             </div>
-            <h1 className='p-2 italic font-medium w-48 line-clamp-1 text-center text-xl'>{name}</h1>
+            <h1 className='p-2 w-48 line-clamp-1 text-center text-xl bg-gray-900'>{name}</h1>
         </button>
     )
 }
