edca30ff5edb9e9f265cb0c4f2e1b7300220374a
TorqueReborn <torquereborn@gmail.com>
2025-08-23 05:26:40 +0530

    We dont need user to know which database it is

--- backend/src/controllers/animeController.js
+++ backend/src/controllers/animeController.js
@@ -13,7 +13,7 @@ export const add = async (req, res) => {
             return res.json({success: false, message: 'Invalid Token Sign in again'})
         }
     } catch (error) {
-        return res.status(404).json({success: false, message: 'Unable to connect to users database'})
+        return res.status(404).json({success: false, message: 'Unable to connect to database'})
     } finally {
         await mongoose.disconnect()
         console.log('Disconnected from users database')
