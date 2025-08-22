aa02dfdf521ad35ed49a2f47d9eb11e1dc192525
TorqueReborn <torquereborn@gmail.com>
2025-08-23 04:49:45 +0530

    Update user schema to take in anime data

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,9 +1,38 @@
 import mongoose, { Schema } from "mongoose";
 
+const animeSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    }
+})
+
 const userSchema = new Schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true},
-    token: {type: String, required: false, default: null}
+    email: {
+        type: String, 
+        required: true
+    },
+    password: {
+        type: String, 
+        required: true
+    },
+    token: {
+        type: String, 
+        required: false, 
+        default: null
+    },
+    anime: {
+        watching: [animeSchema],
+        completed: [animeSchema]
+    }
 })
 
 const User = mongoose.model('User', userSchema)
