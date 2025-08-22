f212936480b4a8bab79e7e9f27546ef426c300f9
TorqueReborn <torquereborn@gmail.com>
2025-08-22 10:46:15 +0530

    Login or Signup Page

--- frontend/src/login/Login.tsx
+++ frontend/src/login/Login.tsx
@@ -10,8 +10,8 @@ interface LoginProps {
     setIsLoggedIn: Function
 }
 
-const login = async (data: Login) => {
-    const response = await fetch('http://localhost:3000/api/auth/login', {
+const login = async (data: Login, isSignUp: boolean) => {
+    const response = await fetch(`http://localhost:3000/api/auth/${isSignUp ? 'register': 'login'}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -21,8 +21,8 @@ const login = async (data: Login) => {
     return await response.json()
 }
 
-const Login = ({setIsLoggedIn}: LoginProps) => {
-
+const Login = ({ setIsLoggedIn }: LoginProps) => {
+    const [isSignUp, setIsSignup] = useState(false)
     const [data, setData] = useState<Login>({
         email: '',
         password: ''
@@ -42,9 +42,13 @@ const Login = ({setIsLoggedIn}: LoginProps) => {
         })
     }
 
+    const handleSignUpChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setIsSignup(e.target.value === 'signup')
+    }
+
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
-        const res = await login(data)
+        const res = await login(data, isSignUp)
         if (res && res.success) {
             Cookies.set('token', res.token)
             setIsLoggedIn(true)
@@ -54,10 +58,19 @@ const Login = ({setIsLoggedIn}: LoginProps) => {
     return (
         <div className='h-screen flex justify-center items-center'>
             <div className='bg-gray-800 items-center justify-center flex flex-col p-12 rounded-xl'>
-                <h1 className='text-3xl'>Login</h1>
-                <input className='mt-8 text-center outline-none border-b-1 w-8/10' type="text" placeholder='Enter email' onChange={handleEmailChange}/><br />
+                <div className="flex gap-2">
+                    <label className={`${!isSignUp ? 'bg-gray-700': ''} px-4 py-2 rounded-xl`}>
+                        Login
+                        <input className='hidden' type="radio" name="sign" id="login" value="login" onChange={handleSignUpChange} />
+                    </label>
+                    <label className={`${isSignUp ? 'bg-gray-700': ''} px-4 py-2 rounded-xl`}>
+                        SignUp
+                        <input className='hidden' type="radio" name="sign" id="signup" value="signup" onChange={handleSignUpChange} />
+                    </label>
+                </div>
+                <input className='mt-8 text-center outline-none border-b-1 w-8/10' type="text" placeholder='Enter email' onChange={handleEmailChange} /><br />
                 <input className='mt-6 text-center outline-none border-b-1 w-8/10' type="password" placeholder='Enter password' onChange={handlePasswordChange} />
-                <button className='mt-6 bg-gray-700 px-4 py-2 cursor-pointer rounded-2xl hover:bg-gray-600 focus:outline-1' type='submit' onClick={handleSubmit}>Login</button>
+                <button className='mt-6 bg-gray-700 px-4 py-2 cursor-pointer rounded-2xl hover:bg-gray-600 focus:outline-1' type='submit' onClick={handleSubmit}>{isSignUp ? 'SignUp' : 'Login'}</button>
             </div>
         </div>
     )
