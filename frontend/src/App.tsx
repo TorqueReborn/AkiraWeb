39604972f8eb4c71a9c12c7366ba894a9e72b1d3
TorqueReborn <torquereborn@gmail.com>
2025-08-25 11:54:16 +0530

    Prepare for major rewrite

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
 import Cookies from 'js-cookie'
 import { useState } from "react";
-import Login from "./login/Login";
 import Splash from "./splash/Splash"
 
 const App = () => {
   const [showSplash, setShowSplash] = useState(!Cookies.get('splashSeen'))
-  const [showLogin, setShowLogin] = useState(!Cookies.get('token'))
 
   return (
     <div>
       {showSplash && <Splash setShowSplash={setShowSplash} />}
-      {showLogin && <Login setShowLogin={setShowLogin} />}
     </div>
   )
 }
