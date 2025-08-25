9e7d8a3fe1dccae915fa2bc24c3b4cd1787fa496
TorqueReborn <torquereborn@gmail.com>
2025-08-25 11:56:08 +0530

    Home Route is required for now

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,11 @@ import './index.css'
 import App from './App.tsx'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Verify from './verify/Verify.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App />} />
-            <Route path='/:token' element={<Verify />} />
         </Routes>
     </BrowserRouter>
 )
