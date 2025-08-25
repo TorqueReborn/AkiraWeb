8a514c3bd1884af774cfd565a16afeb25342fbcc
TorqueReborn <torquereborn@gmail.com>
2025-08-25 08:38:14 +0530

    Router Dom to parse token if received

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,12 @@
 import './index.css'
 import App from './App.tsx'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path='/:token' element={<App />} />
+        </Routes>
+    </BrowserRouter>
+)
