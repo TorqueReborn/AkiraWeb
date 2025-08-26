1d81554ad34a0cfb34f7cc31c8e58f963fe71701
TorqueReborn <torquereborn@gmail.com>
2025-08-26 22:18:53 +0530

    Show latest 5 animes

--- backend/src/controllers/animeController.js
+++ backend/src/controllers/animeController.js
@@ -1,3 +1,11 @@
+import Anime from '../models/animeModel.js'
+
 export const spotlight = async (req, res) => {
+    try {
+        const animes = await Anime.find({}).limit(5)
+        return res.json({success: true, animes: animes})
+    }catch(err) {
+        res.status(404).json({success: false, message: err.message})
+    }
     return res.json({success: true, message: 'Spotlight fetched successfully'})
 }
