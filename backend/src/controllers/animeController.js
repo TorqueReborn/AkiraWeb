72c9905bbd65b83f56409450f9de0cf6d65924fe
TorqueReborn <torquereborn@gmail.com>
2025-08-26 22:20:35 +0530

    If no user added to list return error

--- backend/src/controllers/animeController.js
+++ backend/src/controllers/animeController.js
@@ -3,7 +3,11 @@ import Anime from '../models/animeModel.js'
 export const spotlight = async (req, res) => {
     try {
         const animes = await Anime.find({}).limit(5)
-        return res.json({success: true, animes: animes})
+        if (animes.length > 0) {
+            return res.json({success: true, animes: animes})
+        } else {
+            return res.json({success: false, message: 'No users added animes to their list'})
+        }
     }catch(err) {
         res.status(404).json({success: false, message: err.message})
     }
