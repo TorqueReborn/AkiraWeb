a36d099487caea77af24cd159c9959384dcf80c5
TorqueReborn <torquereborn@gmail.com>
2025-08-26 19:13:19 +0530

    After deletion decrement count too

--- backend/src/controllers/userAnimeController.js
+++ backend/src/controllers/userAnimeController.js
@@ -3,7 +3,7 @@ import User from "../models/userModel.js"
 import Anime from '../models/animeModel.js'
 
 export const addAnime = async (req, res) => {
-    const { token, ids, status = 'watching', progress = 1 } = req.body
+    const { token, ids, status = 'watching', progress = 0 } = req.body
 
     if (!token || !ids || ids.length === 0) {
         return res.status(404).json({ success: false, message: 'Missing parameters' })
@@ -17,12 +17,12 @@ export const addAnime = async (req, res) => {
                 if (userAnime) {
                     await User.updateOne({ token: token, "anime.id": id }, { $set: { "anime.$[element].status": status, "anime.$[element].progress": progress } }, { arrayFilters: [{ "element.id": id }] })
                 } else {
-                    await User.updateOne({token}, {$push: {anime: {id, status, progress}}})
-                    const anime = await Anime.findOne({id})
+                    await User.updateOne({ token }, { $push: { anime: { id, status, progress } } })
+                    const anime = await Anime.findOne({ id })
                     if (anime) {
-                        await Anime.updateOne({id: id}, {$inc: {count: 1}})
+                        await Anime.updateOne({ id: id }, { $inc: { count: 1 } })
                     } else {
-                        await Anime.insertOne({id: id, count: 1})
+                        await Anime.insertOne({ id: id, count: 1 })
                     }
                 }
             });
@@ -36,28 +36,26 @@ export const addAnime = async (req, res) => {
 }
 
 export const deleteAnime = async (req, res) => {
-    const { token, id } = req.body
+    const { token, ids } = req.body
 
-    if (!token || !id) {
+    if (!token || !ids || ids.length === 0) {
         return res.status(404).json({ success: false, message: 'Missing parameters' })
     }
 
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI)
-        console.log('Connected to users database')
-
         const user = await User.findOne({ token })
         if (!user) {
             return res.json({ success: false, message: 'Invalid Token Sign in again' })
         }
-
-        user.anime.pull({ id })
-        await user.save()
+        ids.forEach(async (id) => {
+            const userAnime = await User.findOne({ token: token, "anime.id": id })
+            if (userAnime) {
+                await User.updateOne({ token: token }, { $pull: { anime: { id: id } } })
+                await Anime.updateOne({ id: id, count: { $gt: 0 } }, { $inc: { count: -1 } })
+            }
+        })
         return res.json({ success: true, message: 'User anime deleted' })
     } catch (error) {
         return res.status(404).json({ success: false, message: error.message })
-    } finally {
-        await mongoose.disconnect()
-        console.log('Disconnected from users database')
     }
 }
