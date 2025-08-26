da34f0515a8cd845cc307b2b72d4ed35c1cc0b59
TorqueReborn <torquereborn@gmail.com>
2025-08-26 22:59:25 +0530

    Use fetch to fetch spotlight json

--- frontend/src/pages/Home/Spotlight/Spotlight.tsx
+++ frontend/src/pages/Home/Spotlight/Spotlight.tsx
@@ -1,4 +1,23 @@
+import { useEffect } from "react"
+
+interface Anime {
+  id: string
+}
+
 const Spotlight = () => {
+
+  useEffect(() => {
+    const fetchSpotlight = async () => {
+      const spot = await fetch('http://localhost:3000/api/spotlight')
+      const rawJSON = await spot.json()
+      const ids = rawJSON.animes.map((anime: Anime) => {
+        return {id: anime.id}
+      })
+      console.log(ids)
+    }
+    fetchSpotlight()
+  }, [])
+
   return (
     <div>Spotlight</div>
   )
