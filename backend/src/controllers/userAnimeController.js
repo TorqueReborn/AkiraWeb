6220bf02505e1c4af33d4014ff8838bc3f6b3a29
TorqueReborn <torquereborn@gmail.com>
2025-08-26 11:04:46 +0530

    Update code to reduce server load

--- backend/src/controllers/userAnimeController.js
+++ backend/src/controllers/userAnimeController.js
@@ -2,50 +2,30 @@ import mongoose from "mongoose"
 import User from "../models/userModel.js"
 
 export const addAnime = async (req, res) => {
-    const { token, ids, status = 'watching', progress = 0 } = req.body
+    const { token, ids, status = 'watching', progress = 1 } = req.body
 
-    if (!token || !ids) {
+    if (!token || !ids || ids.length === 0) {
         return res.status(404).json({ success: false, message: 'Missing parameters' })
     }
 
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI)
-        console.log('Connected to users database')
-
         const user = await User.findOne({ token })
-        if (!user) {
-            return res.json({ success: false, message: 'Invalid Token Sign in again' })
+        if (user) {
+            ids.forEach(async (id) => {
+                const anime = await User.findOne({ token: token, "anime.id": id })
+                if (anime) {
+                    await User.updateOne({ token: token, "anime.id": id }, { $set: { "anime.$[element].status": status, "anime.$[element].progress": progress } }, { arrayFilters: [{ "element.id": id }] })
+                } else {
+                    user.anime.push({ id, status, progress })
+                    await user.save()
+                }
+            });
+            return res.json({ success: true })
+        } else {
+            res.status(404).json({ success: false, message: 'Invalid Token' })
         }
-        for (const id of ids) {
-            const anime = await User.findOne({ token, "anime.id": id })
-            if (anime) {
-                await User.findOneAndUpdate(
-                    {
-                        token,
-                        "anime.id": id
-                    },
-                    {
-                        "$set": {
-                            "anime.$[elem].status": status,
-                            "anime.$[elem].progress": progress
-                        }
-                    },
-                    {
-                        new: true,
-                        arrayFilters: [{ "elem.id": id }]
-                    }
-                )
-            } else {
-                user.anime.push({ id, status, progress })
-                await user.save()
-            }
-        }
-        return res.json({ success: true, message: 'User anime(s) approved' })
-    } catch (error) {
-        return res.status(404).json({ success: false, message: error.message })
-    } finally {
-        await mongoose.disconnect()
-        console.log('Disconnected from users database')
+    } catch (err) {
+        return res.status(500).json({ success: false, message: 'An unexpected error occured', error: err.message })
     }
 }
 
@@ -64,7 +44,7 @@ export const deleteAnime = async (req, res) => {
         if (!user) {
             return res.json({ success: false, message: 'Invalid Token Sign in again' })
         }
-        
+
         user.anime.pull({ id })
         await user.save()
         return res.json({ success: true, message: 'User anime deleted' })
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import express from 'express';
 import authRouter from './routers/authRouter.js';
 import userRouter from './routers/userRouter.js';
 import allAnimeRouter from './routers/allAnimeRouter.js';
+import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
@@ -12,9 +13,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cors({ origin: 'http://localhost:5173' }));
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
+mongoose.connect(process.env.MONGO_DB_URI).
+    then(() => {
+        console.log('Connected to database')
+        app.listen(PORT, () => {
+            console.log(`Server is running at http://localhost:${PORT}`)
+        });
+    })
+    .catch(err => {
+        console.error('Database connection error: ', err)
+        process.exit(1)
+    })
 
 app.get('/', async (_, res) => {
     res.json({ success: true, message: 'Welcome to Akira API' })
@@ -24,3 +33,10 @@ app.get('/', async (_, res) => {
 app.use('/api/auth', authRouter)
 app.use('/api/anime', allAnimeRouter)
 app.use('/api/user', userRouter)
+
+process.on('SIGINT', () => {
+    mongoose.disconnect(() => {
+        console.log('Disconnected from database')
+        process.exit(0)
+    })
+})
