a9c950082aab3912fb56c2ef4101fa18876ce967
TorqueReborn <torquereborn@gmail.com>
2025-08-27 13:10:04 +0530

    Watch route and watch algorithm

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import './index.css'
 import App from './App.tsx'
+import Watch from './pages/Watch/Watch.tsx'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
@@ -7,6 +8,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<App />} />
+            <Route path='/:id/:episode' element={<Watch/>}/>
         </Routes>
     </BrowserRouter>
 )
---
+++ frontend/src/pages/Watch/Watch.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { generatePost } from "../../utils"
+
+const Watch = () => {
+    const { id, episode } = useParams()
+    const [videoUri, setVideoUri] = useState()
+
+    useEffect(() => {
+        const fetchVideoUri = async () => {
+            if (id && episode) {
+                const response = await fetch(`http://localhost:3000/api/anime/${id}/${episode}`, generatePost({"translationType": "sub"}))
+                setVideoUri(await response.json())
+            }
+        }
+        fetchVideoUri()
+    }, [])
+
+    return (
+        <div>
+            <video src={videoUri}></video>
+        </div>
+    )
+}
+
+export default Watch
