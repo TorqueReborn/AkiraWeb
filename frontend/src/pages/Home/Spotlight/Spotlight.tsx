257f6d5997bc966bcbc10d1c2c6c5ffed9b43a45
TorqueReborn <torquereborn@gmail.com>
2025-08-27 09:25:12 +0530

    Complete Spotlight Algorithm

--- frontend/src/pages/Home/Spotlight/Spotlight.tsx
+++ frontend/src/pages/Home/Spotlight/Spotlight.tsx
@@ -1,28 +1,39 @@
-import { useEffect } from "react"
+import { useEffect, useState, type ReactElement } from "react"
 import Banner from './components/Banner'
 
 interface Anime {
   _id: string,
   englishName: string,
-  thumbnail: string
+  thumbnail: string,
+  description: string
 }
 
 const Spotlight = () => {
+  const [index, setIndex] = useState(0)
+  const [animeData, setAnimeData] = useState<Anime[]>([])
 
   useEffect(() => {
     const fetchSpotlight = async () => {
       const spot = await fetch('http://localhost:3000/api/spotlight')
       const rawJSON = await spot.json()
       const ids = rawJSON.animes.map((anime: Anime) => {
-        return { _id: anime._id, englishName: anime.englishName, thumbnail: anime.thumbnail }
+        return { _id: anime._id, englishName: anime.englishName, thumbnail: anime.thumbnail, description: anime.description }
       })
+      setAnimeData(ids)
     }
     fetchSpotlight()
   }, [])
 
+  useEffect(() => {
+    if (animeData.length > 0) {
+      const time = setTimeout(() => setIndex((prevIndex) => (prevIndex + 1) % animeData.length), 5000)
+      return () => clearTimeout(time)
+    }
+  }, [animeData, index])
+
   return (
     <div>
-      <Banner img="https://s4.anilist.co/file/anilistcdn/media/anime/banner/184237-SBj6UJvIWAOg.jpg"/>
+      {animeData.length > 0 && <Banner title={animeData[index].englishName} img={animeData[index].thumbnail} description={animeData[index].description}/>}
     </div>
   )
 }
--- frontend/src/pages/Home/Spotlight/components/Banner.tsx
+++ frontend/src/pages/Home/Spotlight/components/Banner.tsx
@@ -1,15 +1,17 @@
 interface BannerProps {
-    img: string
+    title: string,
+    img: string,
+    description: string
 }
 
-const Banner = ({ img }: BannerProps) => {
+const Banner = ({ title, img, description }: BannerProps) => {
     return (
         <div className="relative h-[75vh]">
             <div className="absolute w-full h-full bg-gradient-to-r from-gray-900 to-gray-100/0" />
             <div className="absolute w-full h-full bg-gradient-to-t from-gray-900 to-gray-100/0" />
             <div className="absolute bottom-0 w-1/2 m-20">
-                <h1 className="font-bold text-5xl mb-4">Sakamoto Days</h1>
-                <p className="font-medium line-clamp-3">This is the description of the anime which is a big sentence and will takes up most of the space and i am typing this to ensure that it would only takes up some space and not exceed the permitted space and it will ecenly spreadout and this was supposed to exceed three lines so iam typing even more </p>
+                <h1 className="font-bold text-5xl mb-4">{title}</h1>
+                <p className="font-medium line-clamp-3" dangerouslySetInnerHTML={{__html: description}}/>
                 <button className="px-4 py-2 mx-2 my-4 text-gray-950 bg-amber-300 rounded-full cursor-pointer">Watch</button>
                 <button className="px-4 py-2 mx-2 my-4 text-gray-950 bg-amber-300 rounded-full cursor-pointer">Details</button>
             </div>
