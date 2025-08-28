c08040dd33cc4c79dae7b954fcfd2761fad6fb65
TorqueReborn <torquereborn@gmail.com>
2025-08-28 18:42:00 +0530

    Update Logo Color

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import Search from "./components/Search/Search"
 const Navbar = () => {
     return (
         <div className="flex items-center mx-[2vw] h-[12vh] gap-4 relative">
-            <h1 className="text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500/75 to-purple-500/75 ml-3">
+            <h1 className="text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-amber-500/75 to-amber-400/50 ml-3">
                 Akira
             </h1>
             <div className="absolute right-0">
