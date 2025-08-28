69d84edc102d0e3892efe07fe8cfc643e742155c
TorqueReborn <torquereborn@gmail.com>
2025-08-28 06:29:12 +0530

    Proress synced with time

--- frontend/src/pages/Watch/Player/Player.tsx
+++ frontend/src/pages/Watch/Player/Player.tsx
@@ -1,12 +1,23 @@
 import { useEffect, useRef, useState } from "react"
 import { IoPauseOutline, IoPlayOutline } from "react-icons/io5"
-import Progressbar from "./components/Progressbar"
 
 const Player = ({ videoUri }: { videoUri: string }) => {
     const videoRef = useRef<HTMLVideoElement>(null)
     const [isPlaying, setIsPlaying] = useState(false)
     const [duration, setDuration] = useState<number>(0)
     const [current, setCurrent] = useState<number>(0)
+    const progressRef = useRef<HTMLDivElement>(null)
+    const [progress, setProgress] = useState(0)
+
+    const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (progressRef.current && videoRef.current) {
+            const box = progressRef.current.getBoundingClientRect()
+            const clickedX = e.clientX - box.left
+            const percent = clickedX / box.width
+            setProgress(percent * 100)
+            videoRef.current.currentTime = percent * videoRef.current.duration
+        }
+    }
 
     const formatTime = (timeInSeconds: number) => {
         const minutes = Math.floor(timeInSeconds / 60)
@@ -30,6 +41,7 @@ const Player = ({ videoUri }: { videoUri: string }) => {
     const handleTimeUpdate = () => {
         if (videoRef.current) {
             setCurrent(videoRef.current.currentTime)
+            setProgress(Math.floor((videoRef.current.currentTime / videoRef.current.duration) * 100))
         }
     }
 
@@ -60,7 +72,7 @@ const Player = ({ videoUri }: { videoUri: string }) => {
                 if (videoRef.current) {
                     videoRef.current.volume = videoRef.current.volume + 0.1
                 }
-            }else if (e.key === 'ArrowDown') {
+            } else if (e.key === 'ArrowDown') {
                 e.preventDefault()
                 if (videoRef.current) {
                     videoRef.current.volume = videoRef.current.volume - 0.1
@@ -78,8 +90,10 @@ const Player = ({ videoUri }: { videoUri: string }) => {
             <video ref={videoRef} src={videoUri} className="w-full h-full" onTimeUpdate={handleTimeUpdate} onLoadedMetadata={handleLoadedMetaData} />
             {isPlaying ? <IoPauseOutline className="absolute bottom-1/2 w-8 h-8" onClick={handlePlayPause} /> : <IoPlayOutline className="absolute bottom-1/2 w-8 h-8" onClick={handlePlayPause} />}
             <div className="absolute h-1 bottom-6 w-[45vw] flex items-center">
-                <div className="mr-2">{formatTime(current)}</div>
-                <Progressbar video={videoRef} />
+                <div className="mr-2 w-14">{formatTime(current)}</div>
+                <div ref={progressRef} onClick={handleClick} className="w-full cursor-pointer bg-amber-300">
+                    <div style={{ width: `${progress}%` }} className="h-1 bg-white" />
+                </div>
                 <div className="ml-2">{formatTime(duration)}</div>
             </div>
         </div>
