470b23406b757c251ee656d982eae2686564cc6a
TorqueReborn <torquereborn@gmail.com>
2025-08-28 13:12:17 +0530

    Show three bars for drawer

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,13 @@
+import { VscThreeBars } from "react-icons/vsc"
 import Search from "./components/Search/Search"
 
 const Navbar = () => {
     return (
-        <div className="flex items-center mx-[2vw] h-[10vh] gap-4 relative">
-            <h1 className="text-2xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+        <div className="flex items-center mx-[2vw] h-[12vh] gap-4 relative">
+            <div tabIndex={0} className="cursor-pointer">
+                <VscThreeBars size={38} />
+            </div>
+            <h1 className="text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
                 Akira
             </h1>
             <div className="absolute right-0">
--- frontend/src/components/Navbar/components/Search/Search.tsx
+++ frontend/src/components/Navbar/components/Search/Search.tsx
@@ -2,7 +2,7 @@ import { FaSearch } from "react-icons/fa"
 
 const Search = () => {
     return (
-        <div className="flex px-8">
+        <div className="flex">
             <input type="text" placeholder="Search" className="outline-1 outline-gray-400 rounded-full px-4 py-2 mr-3" />
             <div tabIndex={0} className="bg-amber-300 p-4 rounded-full cursor-pointer">
                 <FaSearch className="text-gray-600" />
