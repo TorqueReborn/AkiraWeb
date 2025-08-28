479ac5ed4e7c81b2174fbbbbbd6e166e15ef7ecb
TorqueReborn <torquereborn@gmail.com>
2025-08-28 09:48:13 +0530

    More componentization

--- frontend/src/pages/Watch/Player/Player.tsx
+++ frontend/src/pages/Watch/Player/Player.tsx
@@ -1,61 +1,15 @@
 import Progressbar from "./components/Progressbar"
-import { useEffect, useRef, useState } from "react"
+import { useRef } from "react"
 import PlayPauseButton from "./components/PlayPauseButton"
 
 const Player = ({ videoUri }: { videoUri: string }) => {
-    const [isPlaying, setIsPlaying] = useState(false)
     const videoRef = useRef<HTMLVideoElement>(null)
-
-    const handlePlayPause = () => {
-        if (videoRef.current) {
-            if (isPlaying) {
-                videoRef.current.pause()
-            } else {
-                videoRef.current.play()
-            }
-            setIsPlaying(!isPlaying)
-        }
-    }
-
-    useEffect(() => {
-        const handleKeyDown = (e: KeyboardEvent) => {
-            console.log(e.key)
-            if (e.key === ' ') {
-                e.preventDefault()
-                handlePlayPause()
-            } else if (e.key === 'ArrowRight') {
-                e.preventDefault()
-                if (videoRef.current) {
-                    videoRef.current.currentTime += 5
-                }
-            } else if (e.key === 'ArrowLeft') {
-                e.preventDefault()
-                if (videoRef.current) {
-                    videoRef.current.currentTime -= 5
-                }
-            } else if (e.key === 'ArrowUp') {
-                e.preventDefault()
-                if (videoRef.current) {
-                    videoRef.current.volume = videoRef.current.volume + 0.1
-                }
-            } else if (e.key === 'ArrowDown') {
-                e.preventDefault()
-                if (videoRef.current) {
-                    videoRef.current.volume = videoRef.current.volume - 0.1
-                }
-            }
-        }
-        document.addEventListener('keydown', handleKeyDown)
-        return () => {
-            document.removeEventListener('keydown', handleKeyDown)
-        }
-    }, [handlePlayPause])
 
     return (
         <div className="relative w-[50vw] flex items-center justify-center">
             <video ref={videoRef} src={videoUri} className="w-full h-full" />
             <div className="absolute bottom-1/2">
-                <PlayPauseButton videoRef={videoRef} playing={isPlaying} setIsPlaying={setIsPlaying}/>
+                <PlayPauseButton videoRef={videoRef}/>
             </div>
             <div className="absolute bottom-12 h-1 w-[45vw]">
                 <Progressbar videoRef={videoRef}/>
--- frontend/src/pages/Watch/Player/components/PlayPauseButton.tsx
+++ frontend/src/pages/Watch/Player/components/PlayPauseButton.tsx
@@ -1,28 +1,60 @@
-import type { Dispatch, RefObject, SetStateAction } from "react"
+import { useEffect, useState, type RefObject } from "react"
 import { IoPauseOutline, IoPlayOutline } from "react-icons/io5"
 
 interface PlayPauseButtonProps {
-    playing: boolean,
-    videoRef: RefObject<HTMLVideoElement | null>,
-    setIsPlaying: Dispatch<SetStateAction<boolean>>
+    videoRef: RefObject<HTMLVideoElement | null>
 }
 
-const PlayPauseButton = ({ playing, videoRef, setIsPlaying }: PlayPauseButtonProps) => {
+const PlayPauseButton = ({ videoRef }: PlayPauseButtonProps) => {
+    const [isPlaying, setIsPlaying] = useState(false)
 
     const handlePlayPause = () => {
         if (videoRef.current) {
-            if (playing) {
+            if (isPlaying) {
                 videoRef.current.pause()
             } else {
                 videoRef.current.play()
             }
-            setIsPlaying(!playing)
+            setIsPlaying(!isPlaying)
         }
     }
 
+    useEffect(() => {
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.key === ' ') {
+                e.preventDefault()
+                handlePlayPause()
+            } else if (e.key === 'ArrowRight') {
+                e.preventDefault()
+                if (videoRef.current) {
+                    videoRef.current.currentTime += 5
+                }
+            } else if (e.key === 'ArrowLeft') {
+                e.preventDefault()
+                if (videoRef.current) {
+                    videoRef.current.currentTime -= 5
+                }
+            } else if (e.key === 'ArrowUp') {
+                e.preventDefault()
+                if (videoRef.current) {
+                    videoRef.current.volume = videoRef.current.volume + 0.1
+                }
+            } else if (e.key === 'ArrowDown') {
+                e.preventDefault()
+                if (videoRef.current) {
+                    videoRef.current.volume = videoRef.current.volume - 0.1
+                }
+            }
+        }
+        document.addEventListener('keydown', handleKeyDown)
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown)
+        }
+    }, [handlePlayPause])
+
     return (
         <div>
-            {playing ? <IoPauseOutline size={50} onClick={handlePlayPause} /> : <IoPlayOutline size={50} onClick={handlePlayPause} />}
+            {isPlaying ? <IoPauseOutline size={50} onClick={handlePlayPause} /> : <IoPlayOutline size={50} onClick={handlePlayPause} />}
         </div>
     )
 }
