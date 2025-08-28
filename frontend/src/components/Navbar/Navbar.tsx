15097523e2a8aa0a8846329324a98e1469718974
TorqueReborn <torquereborn@gmail.com>
2025-08-28 18:18:14 +0530

    We need a logo

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,12 +1,8 @@
-import { VscThreeBars } from "react-icons/vsc"
 import Search from "./components/Search/Search"
 
 const Navbar = () => {
     return (
         <div className="flex items-center mx-[2vw] h-[12vh] gap-4 relative">
-            <div tabIndex={0} className="cursor-pointer">
-                <VscThreeBars size={38} />
-            </div>
             <h1 className="text-4xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
                 Akira
             </h1>
