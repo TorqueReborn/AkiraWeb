725e9a87d74e46adfec87f94682877337dcace79
TorqueReborn <torquereborn@gmail.com>
2025-08-28 13:03:04 +0530

    Modern Search Icon

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,14 @@
+import Search from "./components/Search/Search"
+
 const Navbar = () => {
     return (
-        <div className="flex items-center mx-[2vw] h-[10vh]">
+        <div className="flex items-center mx-[2vw] h-[10vh] gap-4 relative">
             <h1 className="text-2xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
                 Akira
             </h1>
+            <div className="absolute right-0">
+                <Search />
+            </div>
         </div>
     )
 }
---
+++ frontend/src/components/Navbar/components/Search/Search.tsx
@@ -0,0 +1,14 @@
+import { FaSearch } from "react-icons/fa"
+
+const Search = () => {
+    return (
+        <div className="flex px-8">
+            <input type="text" placeholder="Search" className="outline-1 outline-gray-400 rounded-full px-4 py-2 mr-3" />
+            <div tabIndex={0} className="bg-amber-300 p-4 rounded-full cursor-pointer">
+                <FaSearch className="text-gray-600" />
+            </div>
+        </div>
+    )
+}
+
+export default Search
