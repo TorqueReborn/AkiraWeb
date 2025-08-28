1675733bae3615a876140168ae472bcb6ec59ff0
TorqueReborn <torquereborn@gmail.com>
2025-08-28 10:14:56 +0530

    Only show when hovered with mouse

--- frontend/src/pages/Watch/Player/Player.tsx
+++ frontend/src/pages/Watch/Player/Player.tsx
@@ -5,9 +5,11 @@ const Player = ({ videoUri }: { videoUri: string }) => {
     const videoRef = useRef<HTMLVideoElement>(null)
 
     return (
-        <div className="w-[50vw]">
+        <div className="w-[50vw] relative group">
             <video ref={videoRef} src={videoUri} className="w-full h-full" />
-            <Controls videoRef={videoRef}/>
+            <div className="absolute inset-x-0 bottom-0 p-4 transition-all duration-300 opacity-0 group-focus-within:opacity-100 group-hover:opacity-100">
+                <Controls videoRef={videoRef} />
+            </div>
         </div>
     )
 }
