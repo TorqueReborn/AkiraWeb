34d4dfb32e7c1b7e61014aa7bf641281201d1582
TorqueReborn <torquereborn@gmail.com>
2025-09-25 09:54:04 +0530

    backend: Signup route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const authRouter = require('./routes/authRoute')
+
 const PORT = 3000
+
+app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
     res.json({success: true, message: 'Welcome to Akira API'})
---
+++ backend/routes/authRoute.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/signup', (req, res) => {
+    res.json({success: true, message: 'You are inside signup'})
+})
+
+module.exports = router
