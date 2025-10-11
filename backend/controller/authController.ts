ef15bb02c446a15bbdac2845ecc6ef4c76a1eb94
TorqueReborn <torquereborn@gmail.com>
2025-10-11 11:27:06 +0530

    backend: Generate token and signup user

--- backend/controller/authController.ts
+++ backend/controller/authController.ts
@@ -1,6 +1,6 @@
 import UserSchema from "../model/User.ts";
-import connectDB from "../utils.ts";
 import type { Request, Response } from "express";
+import { connectDB, generateHash } from "../utils.ts";
 
 const initialCheck = (req: Request<Record<string, any>>) => !req.body
 
@@ -9,11 +9,19 @@ export const signup = async (
     res: Response<Record<string, any>>
 ) => {
     if (initialCheck(req) && !req.body?.username && !req.body?.password) return res.status(404).send()
-
     const db = connectDB('akira')
     const users = db.model('User', UserSchema)
-    
+    const token = await generateHash(req.body.username)
+    const search = await users.findOne({username: req.body.username})
+    if (search){
+        db.close()
+        return res.status(404).send()
+    }
+    await users.create({
+        username: req.body.username,
+        password: req.body.password,
+        token
+    })
     await db.close()
-
-    return res.json({ message: 'This is signup' })
+    return res.status(200).send()
 }
--- backend/utils.ts
+++ backend/utils.ts
@@ -1,15 +1,15 @@
 import bcrypt from 'bcrypt'
 import mongoose from "mongoose";
 
+const connectDB = (dbName: string) => {
+    const DB_URL = process.env.DB_URL + dbName
+    return mongoose.createConnection(DB_URL)
+}
+
 const generateHash = async (value: string) => {
     const saltRounds = 10
     const hash = await bcrypt.hash(value, saltRounds)
     return hash
 }
 
-const connectDB = (dbName: string) => {
-    const DB_URL = process.env.DB_URL + dbName
-    return mongoose.createConnection(DB_URL)
-}
-
-export default connectDB
+export { connectDB, generateHash }
