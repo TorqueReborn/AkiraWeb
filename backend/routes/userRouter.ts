d8937d1bc2d406c249004a5bda4fd46b0514c835
TorqueReborn <torquereborn@gmail.com>
2025-10-13 17:20:37 +0530

    backend: Use addToSet to remove duplicates. * Insert data using request body

--- backend/routes/userRouter.ts
+++ backend/routes/userRouter.ts
@@ -12,11 +12,11 @@ userRouter.post('/watching', async (req, res) => {
     if (!req.body || !req.body.username || !req.body.token) return res.status(404).send()
     const db = connectDB(`akira_${req.body.username}`)
     const user = db.model('Watching', AnimeSchema)
-    await user.create({
-        id: 'jsdbdj',
-        type: 'watching',
-        watchedEpisodes: [1, 2, 3]
-    })
+    await user.findOneAndUpdate(
+        { id: req.body.id },
+        { $addToSet: { watchedEpisodes: req.body.watched } },
+        { new: true, upsert: true }
+    )
     return res.json({ message: 'You are inside watching anime' })
 })
 
