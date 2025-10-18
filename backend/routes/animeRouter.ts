865df6e0e6d2ed636413446bf1b1de092ef5edb0
TorqueReborn <torquereborn@gmail.com>
2025-10-18 23:13:28 +0530

    backend: Setup get anime by id endpoint

--- backend/routes/animeRouter.ts
+++ backend/routes/animeRouter.ts
@@ -1,9 +1,31 @@
 import express from 'express'
 
 const animeRouter = express.Router()
+const API_END_POINT = "https://api.allanime.day/api"
 
-animeRouter.get('/', (req, res) => {
-    return res.json({message: 'This is this'})
+animeRouter.get('/:id', async (req, res) => {
+    const QUERY = `
+        query($_id:String!) {
+            show(_id:$_id) {
+                name,
+                englishName,
+                thumbnail
+            }
+        }
+    `
+    const VARIABLES = {
+        "_id": req.params.id
+    }
+    const response = await fetch(API_END_POINT, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            query: QUERY,
+            variables: VARIABLES
+        })
+    })
+    const json = await response.json()
+    return res.json(json)
 })
 
 export default animeRouter
