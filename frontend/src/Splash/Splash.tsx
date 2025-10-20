6aafea4e811d3ec06f9b65bc2520569fe804dc2b
TorqueReborn <torquereborn@gmail.com>
2025-10-20 21:35:44 +0530

    frontend: Show arrow right

--- frontend/src/Splash/Splash.tsx
+++ frontend/src/Splash/Splash.tsx
@@ -1,9 +1,16 @@
+import { MdKeyboardArrowRight } from "react-icons/md";
+
 const Splash = () => {
   return (
-    <div className="flex items-center justify-center h-screen gap-4">
-        <img src="/logo.png" width={100} />
-        <div className="text-6xl font-black bg-linear-to-r from-indigo-800 via-blue-400 to-purple-400 text-transparent bg-clip-text">
-          Akira
+    <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex items-center gap-4">
+          <img src="/logo.png" width={100} />
+          <div className="text-6xl font-black bg-linear-to-r from-indigo-800 via-blue-400 to-purple-400 text-transparent bg-clip-text">
+            Akira
+          </div>
+        </div>
+        <div className="bg-white mt-15 rounded-full">
+          <MdKeyboardArrowRight size={45} className="text-black"/>
         </div>
     </div>
   )
