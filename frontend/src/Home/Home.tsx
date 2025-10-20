33d2f7a8b5f4a55f0577e120c2d90fc61519c25b
TorqueReborn <torquereborn@gmail.com>
2025-10-21 02:35:35 +0530

    frontend: Check trending anime api

--- frontend/src/Home/Home.tsx
+++ frontend/src/Home/Home.tsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react"
+
 const Home = () => {
+  useEffect(() => {
+    const getData = async () => {
+      const test = await fetch(`${import.meta.env.VITE_BACK_END_URL}/anime/trending`)
+      const json = await test.json()
+      console.log(json)
+    }
+    getData()
+  }, [])
   return (
     <div>
-        
+
     </div>
   )
 }
