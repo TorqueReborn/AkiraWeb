4b831dfd21dcf67866e896a82a63533cb9786df2
TorqueReborn <torquereborn@gmail.com>
2025-10-21 00:47:55 +0530

    backend: Functionize response from server

--- backend/routes/animeRouter.ts
+++ backend/routes/animeRouter.ts
@@ -3,6 +3,19 @@ import express from 'express'
 const animeRouter = express.Router()
 const API_END_POINT = "https://api.allanime.day/api"
 
+const getResponseJSON = async (QUERY: string, VARIABLES: object) => {
+    const response = await fetch(API_END_POINT, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            query: QUERY,
+            variables: VARIABLES
+        })
+    })
+    console.log(response)
+    return await response.json()
+}
+
 animeRouter.get('/ids', async (req, res) => {
     let QUERY = ''
     let VARIABLES = {}
@@ -33,15 +46,7 @@ animeRouter.get('/ids', async (req, res) => {
             "_id": req.query.ids
         }
     }
-    const response = await fetch(API_END_POINT, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            query: QUERY,
-            variables: VARIABLES
-        })
-    })
-    const json = await response.json()
+    const json = await getResponseJSON(QUERY, VARIABLES)
     return res.json(json)
 })
 
@@ -62,15 +67,7 @@ animeRouter.get('/trending', async (req, res) => {
             "sortBy": "Recent"
         }
     }
-    const response = await fetch(API_END_POINT, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            query: QUERY,
-            variables: VARIABLES
-        })
-    })
-    const json = await response.json()
+    const json = await getResponseJSON(QUERY, VARIABLES)
     return res.json(json)
 })
 
