b0491bf9d33bdbae12af30fde5acc7284b674361
TorqueReborn <torquereborn@gmail.com>
2025-10-20 21:12:20 +0530

    frontend: Stylize Logo and Logo Text

--- frontend/src/Splash/Splash.tsx
+++ frontend/src/Splash/Splash.tsx
@@ -1,8 +1,10 @@
 const Splash = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-        <img src="/logo.png" />
-        Akira
+    <div className="flex items-center justify-center h-screen gap-4">
+        <img src="/logo.png" width={100} />
+        <div className="text-6xl font-black bg-linear-to-r from-indigo-800 via-blue-400 to-purple-400 text-transparent bg-clip-text">
+          Akira
+        </div>
     </div>
   )
 }
