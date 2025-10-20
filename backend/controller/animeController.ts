72a70f7731cd84ccf3e4cc1388c9ef2d097806f5
TorqueReborn <torquereborn@gmail.com>
2025-10-21 00:54:11 +0530

    backend: Controllerize anime router

---
+++ backend/controller/animeController.ts
@@ -0,0 +1,72 @@
+import type { Request, Response } from "express"
+
+const REQUIRED_DATA = "name,englishName,thumbnail"
+const API_END_POINT = "https://api.allanime.day/api"
+
+const getResponseJSON = async (QUERY: string, VARIABLES: object) => {
+    const response = await fetch(API_END_POINT, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            query: QUERY,
+            variables: VARIABLES
+        })
+    })
+    console.log(response)
+    return await response.json()
+}
+
+export const ids = async (
+    req: Request<Record<string, any>>,
+    res: Response<Record<string, any>>
+) => {
+    let QUERY = ''
+    let VARIABLES = {}
+    if (Array.isArray(req.query.ids)) {
+        QUERY = `
+            query($ids: [String!]!){
+                showsWithIds(ids: $ids) {
+                    ${REQUIRED_DATA}
+                }
+            }
+        `
+        VARIABLES = {
+            "ids": req.query.ids
+        }
+    } else {
+        QUERY = `
+            query($_id:String!) {
+                show(_id:$_id) {
+                    ${REQUIRED_DATA}
+                }
+            }
+        `
+        VARIABLES = {
+            "_id": req.query.ids
+        }
+    }
+    const json = await getResponseJSON(QUERY, VARIABLES)
+    return res.json(json)
+}
+
+export const trending = async (
+    req: Request<Record<string, any>>,
+    res: Response<Record<string, any>>
+) => {
+    const QUERY = `
+        query($search: SearchInput!){
+            shows(search: $search) {
+                edges {
+                    ${REQUIRED_DATA}
+                }
+            }
+        }
+    `
+    const VARIABLES = {
+        "search": {
+            "sortBy": "Recent"
+        }
+    }
+    const json = await getResponseJSON(QUERY, VARIABLES)
+    return res.json(json)
+}
--- backend/routes/animeRouter.ts
+++ backend/routes/animeRouter.ts
@@ -1,69 +1,14 @@
 import express from 'express'
+import { ids, trending } from '../controller/animeController.ts'
 
 const animeRouter = express.Router()
-const API_END_POINT = "https://api.allanime.day/api"
-const REQUIRED_DATA = "name,englishName,thumbnail"
-
-const getResponseJSON = async (QUERY: string, VARIABLES: object) => {
-    const response = await fetch(API_END_POINT, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            query: QUERY,
-            variables: VARIABLES
-        })
-    })
-    console.log(response)
-    return await response.json()
-}
 
 animeRouter.get('/ids', async (req, res) => {
-    let QUERY = ''
-    let VARIABLES = {}
-    if (Array.isArray(req.query.ids)) {
-        QUERY = `
-            query($ids: [String!]!){
-                showsWithIds(ids: $ids) {
-                    ${REQUIRED_DATA}
-                }
-            }
-        `
-        VARIABLES = {
-            "ids": req.query.ids
-        }
-    } else {
-        QUERY = `
-            query($_id:String!) {
-                show(_id:$_id) {
-                    ${REQUIRED_DATA}
-                }
-            }
-        `
-        VARIABLES = {
-            "_id": req.query.ids
-        }
-    }
-    const json = await getResponseJSON(QUERY, VARIABLES)
-    return res.json(json)
+    ids(req, res)
 })
 
 animeRouter.get('/trending', async (req, res) => {
-    const QUERY = `
-        query($search: SearchInput!){
-            shows(search: $search) {
-                edges {
-                    ${REQUIRED_DATA}
-                }
-            }
-        }
-    `
-    const VARIABLES = {
-        "search": {
-            "sortBy": "Recent"
-        }
-    }
-    const json = await getResponseJSON(QUERY, VARIABLES)
-    return res.json(json)
+    trending(req, res)
 })
 
 export default animeRouter
