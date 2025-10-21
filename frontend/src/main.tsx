96974d24bbf8ab588fada9d7423cde1d0fd20753
TorqueReborn <torquereborn@gmail.com>
2025-10-22 04:09:25 +0530

    frontend: Watch route to watch anime with id

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Watch from './Pages/Watch/Watch.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <div>404 Not Found</div>
+  }, {
+    path: '/watch/:id',
+    element: <Watch/>
   }
 ])
 
