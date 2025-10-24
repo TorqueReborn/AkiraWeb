cc8401cca8a324af4554c5c82c267222d906aa26
TorqueReborn <torquereborn@gmail.com>
2025-10-24 09:13:30 +0530

    frontend: Fetch anime response from backend

--- frontend/src/Pages/Watch/Watch.tsx
+++ frontend/src/Pages/Watch/Watch.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+
 const Watch = () => {
+  const params = useParams()
+  
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/anime/ids?ids=${params.id}`)
+      const edges = await response.json()
+      console.log(edges)
+    }
+    getData()
+  }, [])
+
   return (
     <div>Watch</div>
   )
