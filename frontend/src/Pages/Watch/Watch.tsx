08431243ab46e0537b59951a07fe1cdc3e1c7a4a
TorqueReborn <torquereborn@gmail.com>
2025-10-26 08:49:47 +0530

    frontend: Show video after getting response from server

--- frontend/src/Pages/Watch/Watch.tsx
+++ frontend/src/Pages/Watch/Watch.tsx
@@ -1,19 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 const Watch = () => {
+  const [videoURL, setVideoURL] = useState<string>("")
   const params = useParams()
   
   useEffect(() => {
     const getData = async () => {
       const response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/anime/episode?id=${params.id}&type=sub&episode=${params.episode}`)
-      const edges = await response.json()
+      const json = await response.json()
+      setVideoURL(json.videoURL)
     }
     getData()
   }, [])
 
   return (
-    <div>Watch</div>
+    <div>
+      <video src={videoURL} controls={true}></video>
+    </div>
   )
 }
 
