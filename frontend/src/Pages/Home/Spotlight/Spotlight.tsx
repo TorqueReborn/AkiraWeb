3e5aa38695921257f19abf45a4645b3f48cb2a20
TorqueReborn <torquereborn@gmail.com>
2025-10-27 21:44:33 +0530

    frontend: Show banner based on index

--- frontend/src/Pages/Home/Spotlight/Spotlight.tsx
+++ frontend/src/Pages/Home/Spotlight/Spotlight.tsx
@@ -1,16 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+
+interface Anime {
+    name: string,
+    banner: string
+}
 
 const Spotlight = () => {
+    const [index, setIndex] = useState(0)
+    const [banners, setBanners] = useState<Anime[]>()
+
     useEffect(() => {
         const getData = async () => {
             const response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/anime/trending`)
-            
+            const json = await response.json()
+            setBanners(json.map((j: Anime) => j))
         }
         getData()
     }, [])
 
     return (
         <div>
+            {banners && <img src={banners[index].banner} alt="Anime Banner" />}
         </div>
     )
 }
