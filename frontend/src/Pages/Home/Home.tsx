5f902931e09cb598c9d3f00fbd37dbab2eec7ad6
TorqueReborn <torquereborn@gmail.com>
2025-10-27 19:58:53 +0530

    frontend: Create spotlight component

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,30 +1,10 @@
-import type { Anime } from "../../Anime"
-import Card from "./components/Card"
-import { useEffect, useState } from "react"
+import Spotlight from "./Spotlight/Spotlight"
 
 const Home = () => {
-  const [anime, setAnime] = useState<Anime[]>()
 
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/anime/trending`)
-      const edges = await response.json()
-      const animes: Anime[] = edges.map((edge: any) => ({
-        id: edge._id,
-        name: edge.englishName,
-        thumbnail: edge.thumbnail
-      }))
-      setAnime(animes)
-    }
-    getData()
-  }, [])
   return (
     <div>
-      <div className="flex gap-4">
-        {anime?.map(ani => (
-          <Card id={ani.id} name={ani.name} thumbnail={ani.thumbnail} />
-        ))}
-      </div>
+      <Spotlight />
     </div>
   )
 }
---
+++ frontend/src/Pages/Home/Spotlight/Spotlight.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+
+const Spotlight = () => {
+    useEffect(() => {
+        const getData = async () => {
+            const response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/anime/trending`)
+            
+        }
+        getData()
+    }, [])
+
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Spotlight
