a490ba440064d73fbc7336b2fb252de5e4cf8140
TorqueReborn <torquereborn@gmail.com>
2025-10-29 14:54:12 +0530

    frontend: simple margin top of 4 dp to fix mis align

--- frontend/src/Pages/Home/Trending/Trending.tsx
+++ frontend/src/Pages/Home/Trending/Trending.tsx
@@ -21,7 +21,7 @@ const Trending = () => {
     }, [])
 
     return (
-        <div className="flex overflow-scroll scrollbar-hide">
+        <div className="flex overflow-scroll scrollbar-hide mt-4">
             {anime?.map(an => <Card name={an.name} thumbnail={an.thumbnail}/>)}
         </div>
     )
