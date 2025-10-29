75433c88e35419e22baa102b384ccb32d0ed9807
TorqueReborn <torquereborn@gmail.com>
2025-10-29 16:19:50 +0530

    frontend: Create details component and route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Watch from './Pages/Watch/Watch.tsx'
+import Details from './Pages/Details/Details.tsx'
 
 const router = createBrowserRouter([
   {
@@ -11,8 +12,11 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <div>404 Not Found</div>
   }, {
+    path: '/:id',
+    element: <Details />
+  }, {
     path: '/watch/:id/:episode',
-    element: <Watch/>
+    element: <Watch />
   }
 ])
 
