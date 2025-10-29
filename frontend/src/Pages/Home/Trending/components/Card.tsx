0a54c3007e16e10a8bc017755c0cb209d4484372
TorqueReborn <torquereborn@gmail.com>
2025-10-29 16:36:39 +0530

    frontend: Update Trending style

--- frontend/src/Pages/Home/Trending/components/Card.tsx
+++ frontend/src/Pages/Home/Trending/components/Card.tsx
@@ -15,8 +15,8 @@ const Card = ({ id, name, thumbnail }: CardProps) => {
 
   return (
     <div className="w-52 relative ml-8 flex-none" onClick={handleClick}>
-      {<img src={thumbnail} alt={name} />}
-      <div className="absolute bottom-3 -left-3 origin-left -rotate-90 line-clamp-1">
+      {<img src={thumbnail} alt={name} className="h-72" />}
+      <div className="absolute -bottom-3 -left-3 origin-left -rotate-90 line-clamp-1">
         {name}
       </div>
     </div>
