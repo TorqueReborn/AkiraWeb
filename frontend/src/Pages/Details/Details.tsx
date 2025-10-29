4fc42892d7884fbef07032417285c0111b0942c6
TorqueReborn <torquereborn@gmail.com>
2025-10-29 16:33:48 +0530

    frontend: Get response from backend using id

--- frontend/src/Pages/Details/Details.tsx
+++ frontend/src/Pages/Details/Details.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react"
+import { useParams } from "react-router-dom"
+
 const Details = () => {
+  const params = useParams()
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(`${import.meta.env.VITE_BACK_END_URL}/anime/ids?ids=${params.id}`)
+      const json = await response.json()
+      console.log(json)
+    }
+    getData()
+  }, [])
+
   return (
-    <div>Details</div>
+    <div>
+      {params.id}
+    </div>
   )
 }
 
